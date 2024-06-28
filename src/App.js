4148f5ec989cb88279f8d4961b64e045cd1cd1de
yash <slayycasm997@gmail.com>
2024-06-28 18:13:02 +0530

    Pagination Component is completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import {Toaster,toast} from 'sonner'
-import SearchHook from './hooks/search';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AutoComplete from './pages/autocomplete/AutoComplete';
+import UseCallback from './pages/useCallback/UseCallback';
+import UseRef from './pages/useRef/UseRef';
+import Pagination from './pages/pagination/Pagination';
 
 function App() {
   return (
-    <>
-    <div className='autocomplete'>
-      <div className='heading'>AutoComplete</div>
-      <SearchHook/>
-    </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/autocomplete" element={<AutoComplete/>}/>
+          <Route path="/usecallback" element={<UseCallback/>}/>
+          <Route path="/useref" element={<UseRef/>}/>
+          <Route path="/pagination/:id" element={<Pagination/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
